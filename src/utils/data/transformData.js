ae55b7e34c6ffe03f73722fea36fca56bec279c8
Benjamin <benjismit@live.nl>
2018-02-19 14:02:19 +0100

    add basic attributes

--- src/utils/data/transformData.js
+++ src/utils/data/transformData.js
@@ -3,6 +3,12 @@ import * as watchAttributes from '../../_secret/watchAttributes';
 
 const transformWatchData = (data) => {
   console.log(data);
+
+  const watchFunctions = [];
+  for(let i in data.watchFunctions) {
+    if(data.watchFunctions[i]) watchFunctions.push(i);
+  }
+
   const output = {
     name: `${data.watchBrand.value} ${data.watchModel.value}`,
     type: 'simple',
@@ -16,12 +22,70 @@ const transformWatchData = (data) => {
     attributes: [
       {
         id: watchAttributes.BRAND,
-        name: 'Brand',
-        slug: 'watch-brand',
         visible: true,
-        option: data.watchBrand.value
-      }
-    ]
+        options: [data.watchBrand.value]
+      },
+      {
+        id: watchAttributes.BRACELET_COLOR,
+        visible: true,
+        options: [data.watchBraceletColor.value]
+      },
+      {
+        id: watchAttributes.BRACELET_MATERIAL,
+        visible: true,
+        options: [data.watchBraceletMaterial.value]
+      },
+      {
+        id: watchAttributes.CASE_MATERIAL,
+        visible: true,
+        options: [data.watchCaseMaterial.value]
+      },
+      {
+        id: watchAttributes.CLASP,
+        visible: true,
+        options: [data.watchClasp.value]
+      },
+      {
+        id: watchAttributes.CLASP_MATERIAL,
+        visible: true,
+        options: [data.watchClaspMaterial.value]
+      },
+      {
+        id: watchAttributes.CONDITION,
+        visible: true,
+        options: [data.watchCondition.value]
+      },
+      {
+        id: watchAttributes.DIAL,
+        visible: true,
+        options: [data.watchDial.value] 
+      },
+      {
+        id: watchAttributes.DIAL_NUM,
+        visible: true,
+        options: [data.watchDialNumerals.value]
+      },
+      {
+        id: watchAttributes.FUNCTIONS,
+        visible: true,
+        options: watchFunctions
+      },
+      {
+        id: watchAttributes.DOMESTIC_DELIVERY,
+        visible: true,
+        options: [data.watchDomesticEstimate.value]
+      },
+      {
+        id: watchAttributes.CONTINENTAL_DELIVERY,
+        visible: true,
+        options: [data.watchContinentalEstimate.value]
+      },
+      {
+        id: watchAttributes.WORLDWIDE_DELIVERY,
+        visible: true,
+        options: [data.watchWorldwideEstimate.value]
+      },
+    ],
   };
 
   return output;
